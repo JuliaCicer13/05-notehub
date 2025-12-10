4ccdbf94095f2aef83fddfb2ee423fc25f1c2c94
Yuliia <ciceron131313@gmail.com>
2025-12-10 22:24:18 +0200

    f

--- src/types/note.ts
+++ src/types/note.ts
@@ -1,8 +1,10 @@
+export type NoteTag = "Todo" | "Work" | "Personal" | "Meeting" | "Shooping";
+
 export interface Note {
      title: string;
      id: number,
      content: string,
      createdAt: number,
      updatedAt: number,
-     tag: string,
+     tag: NoteTag,
 }
